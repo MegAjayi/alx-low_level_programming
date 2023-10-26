5a6f71798b36bcd6378bbb0d9b88bfaaf7071c49
MegAjayi <megajayi93@gmail.com>
2023-10-25 22:34:06 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - main function
+ *
+ * @s: Function parameter
+ *
+ * Return: lenght
+ */
+int _strlen_recursion(char *s)
+{
+	int Int = 0;
+
+	if (*s)
+	{
+		Int++;
+		Int += _strlen_recursion(s + 1);
+	}
+	return (Int);
+}
