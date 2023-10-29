c19e5b0dace73aaf208ea0fead79141bbf2142d2
MegAjayi <megajayi93@gmail.com>
2023-10-29 03:05:30 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet - function to print
+*
+* @n: functio parameter
+*
+* Return: always return 0 on sucesses
+*/
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar (n);
+	}
+	_putchar ('\n');
+}
