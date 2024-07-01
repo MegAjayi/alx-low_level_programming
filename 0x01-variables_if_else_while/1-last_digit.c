6d26c8c0f3b6e87912ce9b6f007ebff3ed598f1f
MegAjayi <megajayi93@gmial.com>
2024-07-01 04:46:36 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,9 +21,9 @@ int main(void)
 	}
 	else if (ld < 6 && ld != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
+		printf("Last digit of %d is %d  is less than 6 and not 0\n", n, ld);
 	}
 	else
-		printf("last digit of %d is %d and is 0\n", n, ld);
+		printf("last digit of %d is %d  is 0\n", n, ld);
 	return (0);
 }
