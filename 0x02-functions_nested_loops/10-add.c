df8e236716e3c1ac84c91650c5ab2592a83faa95
MegAjayi <megajayi93@gmial.com>
2024-07-03 11:08:42 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - functionname in tis program
+ * Description: to add two function with each other
+ * @n1: paremeter one
+ * @n2: paremeter two
+ * Return: sum on success
+ */
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
