0e9c6e3374485cd075cb47eb03611c7c3aaad246
MegAjayi <megajayi93@gmial.com>
2024-07-08 02:15:41 -0700

    2-m

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - function in the programe
+ * Description: to multiply two different int
+ * @a: paremeter one
+ * @b: paremeter two
+ * Return: result an int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
