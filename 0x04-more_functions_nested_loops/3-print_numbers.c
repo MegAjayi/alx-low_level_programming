cb58c249b43fbad8eafd19e994b75c147828f9ef
MegAjayi <megajayi93@gmial.com>
2024-07-08 02:25:39 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function name
+ * Description:to print number
+ * Retunr: 0 on sucess
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
